85f1ad49708d450d8304a9539d37d478b9e5fc75
Diego Marcillo Pinzon <dfmarcillop@gmail.com>
2020-05-03 22:34:48 -0500

    feat: added routes from react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from "react";
 import "./App.css";
-import CatData from "./container/catdata";
+import { ThemeProvider } from "styled-components";
 import NavBar from "./components/NavBar/NavBar";
+import { GlobalStyles } from "./components/Common/GlobalStyled";
+import Home from "./components/Home/home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Breeds from "./container/Breeds";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/:page?">
+        <ThemeProvider
+          theme={{
+            fontFamily: "Roboto, sans-serif",
+          }}
+        >
           <NavBar />
-          <CatData />
-        </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <br />
+          <Route path="/breeds" exact>
+            <Breeds />
+          </Route>
+          <GlobalStyles primaryColor />
+        </ThemeProvider>
       </Router>
     </div>
   );
